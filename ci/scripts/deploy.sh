d238528d8f7551073f5a991263201a4a0a0e96e5
alper numan genc <anumangenc@gmail.com>
2019-05-26 21:12:32 +0300

    counting down

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -14,8 +14,8 @@ DCK_TMP=source-code/ci/templates/docker-compose.tmp.yml
 IMAGE_REPOSITORY=$(cat app-image/repository)
 IMAGE_DIGEST=$(cat app-image/digest)
 
-IMAGE="${IMAGE_REPOSITORY}@${IMAGE_DIGEST}"
-EXPORT IMAGE
+export IMAGE="${IMAGE_REPOSITORY}@${IMAGE_DIGEST}"
+
 GIT_SHA=$(cat source-code/.git/ref)
 
 echo "Git SHA:        ${GIT_SHA}"
@@ -24,3 +24,5 @@ echo "VERSION:        ${VERSION}"
 # eval "cat <<< \"$(<source-code/ci/templates/docker-compose.tmp.yml)\" > docker-compose.yml" 2> /dev/null
 envsubst < source-code/ci/templates/docker-compose.tmp.yml > docker-compose.yml
 cat docker-compose.yml
+
+echo "Git SHA:        ${IMAGE}"
