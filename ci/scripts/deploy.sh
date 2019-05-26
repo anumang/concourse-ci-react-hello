c42b7c00f9bb03c95a0a72e4cc8e18efe46c3cff
alper numan genc <anumangenc@gmail.com>
2019-05-26 13:36:02 +0300

    add debug deploy logs

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-CI_DIR=source-code/ci
-
-IMAGE_REPOSITORY=$(cat app-image/repository)
-IMAGE_DIGEST=$(cat app-image/digest)
-
-IMAGE="${IMAGE_REPOSITORY}@${IMAGE_DIGEST}"
-REPLICAS=2
-GIT_SHA=$(cat source-code/.git/ref)
-
 echo "Application:    ${APP}"
 echo "Namespace:      ${NAMESPACE}"
 echo "Environment:    ${ENVIRONMENT}"
-echo "Image:          ${IMAGE}"
-echo "Port:           ${PORT}"
-echo "Git SHA:        ${GIT_SHA}"
-echo "VERSION:        ${VERSION}"
 echo "Swarm:          ${SWARM_HOST}"
+echo "Port:           ${PORT}"
 
-rm -f docker-compose.yml temp.yml  
-( echo "cat <docker-compose.yml";
-  cat source-code/ci/templates/docker-compose.tmp.yml;
-  echo "EOF";
-) >temp.yml
-. temp.yml
-cat docker-compose.yml
+#CI_DIR=source-code/ci
+
+#IMAGE_REPOSITORY=$(cat app-image/repository)
+#IMAGE_DIGEST=$(cat app-image/digest)
+
+#IMAGE="${IMAGE_REPOSITORY}@${IMAGE_DIGEST}"
+#REPLICAS=2
+#GIT_SHA=$(cat source-code/.git/ref)
+
+#echo "Application:    ${APP}"
+#echo "Namespace:      ${NAMESPACE}"
+#echo "Environment:    ${ENVIRONMENT}"
+#echo "Image:          ${IMAGE}"
+#echo "Port:           ${PORT}"
+#echo "Git SHA:        ${GIT_SHA}"
+#echo "VERSION:        ${VERSION}"
+#echo "Swarm:          ${SWARM_HOST}"
+
+#rm -f docker-compose.yml temp.yml  
+#( echo "cat <docker-compose.yml";
+#  cat source-code/ci/templates/docker-compose.tmp.yml;
+#  echo "EOF";
+#) >temp.yml
+#. temp.yml
+#cat docker-compose.yml
