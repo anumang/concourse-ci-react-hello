42c1b4b75a652899249401ad6992a74c93b79875
alper numan genc <anumangenc@gmail.com>
2019-05-26 19:51:23 +0300

    counting down

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -6,28 +6,21 @@ echo "Environment:    ${ENVIRONMENT}"
 echo "Swarm:          ${SWARM_HOST}"
 echo "Port:           ${PORT}"
 
-#CI_DIR=source-code/ci
+CI_DIR=source-code/ci
 
-#IMAGE_REPOSITORY=$(cat app-image/repository)
-#IMAGE_DIGEST=$(cat app-image/digest)
+IMAGE_REPOSITORY=$(cat app-image/repository)
+IMAGE_DIGEST=$(cat app-image/digest)
 
-#IMAGE="${IMAGE_REPOSITORY}@${IMAGE_DIGEST}"
-#REPLICAS=2
-#GIT_SHA=$(cat source-code/.git/ref)
+IMAGE="${IMAGE_REPOSITORY}@${IMAGE_DIGEST}"
+GIT_SHA=$(cat source-code/.git/ref)
 
-#echo "Application:    ${APP}"
-#echo "Namespace:      ${NAMESPACE}"
-#echo "Environment:    ${ENVIRONMENT}"
-#echo "Image:          ${IMAGE}"
-#echo "Port:           ${PORT}"
-#echo "Git SHA:        ${GIT_SHA}"
-#echo "VERSION:        ${VERSION}"
-#echo "Swarm:          ${SWARM_HOST}"
+echo "Git SHA:        ${GIT_SHA}"
+echo "VERSION:        ${VERSION}"
 
-#rm -f docker-compose.yml temp.yml  
-#( echo "cat <docker-compose.yml";
-#  cat source-code/ci/templates/docker-compose.tmp.yml;
-#  echo "EOF";
-#) >temp.yml
-#. temp.yml
-#cat docker-compose.yml
+rm -f docker-compose.yml temp.yml  
+( echo "cat <docker-compose.yml";
+  cat source-code/ci/templates/docker-compose.tmp.yml;
+  echo "EOF";
+) >temp.yml
+. temp.yml
+cat docker-compose.yml
